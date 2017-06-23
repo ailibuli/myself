a37b78de7f5f448049ff6bb49ec01e63c4db6340
zhaomingyang <zhaomingyang@tvmining.com>
2017-06-23 11:53:40 +0800

    laravel52

---
+++ app/Http/Controllers/laravel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class laravel extends Controller
+{
+    public function index( Request $request){
+        
+    }
+}
